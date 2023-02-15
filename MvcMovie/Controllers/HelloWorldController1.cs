735642319e5ef64bb5e088c918aae71e6cf1f9ff
Shrey Talreja <shreytalreja25@gmail.com>
2023-02-15 11:56:16 +0530

    Initial Commit

---
+++ MvcMovie/Controllers/HelloWorldController1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome(string name, int num)
+    {
+        //return "This is the Welcome action method...";
+        return HtmlEncoder.Default.Encode($"Welcome {name}, Number of Visits : {num}");
+    }
+}
